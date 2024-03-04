2b4d0b40eebbdc52bb3d51eca9023e1a01089e76
Anton Kostyuchik <a.kostyuchik@itransition.com>
2024-03-04 21:00:51 +0400

    SHULTSEDU-1944: finished section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,9 +9,12 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddControllers();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(corsBuilder => 
+    corsBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
