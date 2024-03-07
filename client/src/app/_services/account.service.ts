38ca64dab5daf9c2ea941c32719cba7ccde20fee
Anton Kostyuchik <a.kostyuchik@itransition.com>
2024-03-07 17:59:01 +0400

    SHULTSEDU-1944: finished section 6

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -15,8 +15,7 @@ export class AccountService {
 
   login(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
-      map((response: User) => {
-        const user = response;
+      map(user => {
         if(user){
           localStorage.setItem('user', JSON.stringify(user));
           this.currenUserSource.next(user);
